07aea771c92e74a3e5cf89cc5cb589c91957ea96
endlessmomo <soohyuk96@naver.com>
2022-09-06 15:34:10 +0900

    간단한 결제 및 결제 취소 기능 구현

---
+++ src/main/java/com/zerobase/convpay/UserClient.java
@@ -0,0 +1,27 @@
+package com.zerobase.convpay;
+
+import com.zerobase.convpay.dto.PayCancelRequest;
+import com.zerobase.convpay.dto.PayCancelResponse;
+import com.zerobase.convpay.dto.PayRequest;
+import com.zerobase.convpay.dto.PayResponse;
+import com.zerobase.convpay.service.ConveniencePayService;
+import com.zerobase.convpay.type.ConvenienceType;
+
+public class UserClient {
+    public static void main(String[] args) {
+        // '사용자' -> 편결이 -> 머니
+        ConveniencePayService conveniencePayService = new ConveniencePayService();
+
+        // 결제 1000원
+        PayRequest payRequest = new PayRequest(ConvenienceType.G25, 1000);
+        PayResponse payResponse = conveniencePayService.pay(payRequest);
+
+        System.out.println(payResponse.toString());
+
+        // 취소 500원
+        PayCancelRequest payCancelRequest = new PayCancelRequest(ConvenienceType.G25, 500);
+        PayCancelResponse payCancelResponse = conveniencePayService.payCancel(payCancelRequest);
+
+        System.out.println(payCancelResponse.toString());
+    }
+}
