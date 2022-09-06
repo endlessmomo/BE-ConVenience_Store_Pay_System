9a3ae4075a2b8cb9adcf981f683710bb165817a5
endlessmomo <soohyuk96@naver.com>
2022-09-06 10:57:33 +0900

    기본 환경 설정

---
+++ src/main/java/com/zerobase/convpay/ConvpayApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.convpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/convpay/ConvpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.convpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
